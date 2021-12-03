97af972a2c52d6457e908ea2a6c522072ecc3441
Murat Karasah <mkarasah@gmail.com>
2021-12-03 10:49:19 +0300

    >kare ve küp

--- refOutParametresiConsoleApp/Program.cs
+++ refOutParametresiConsoleApp/Program.cs
@@ -4,18 +4,25 @@ namespace refOutParametresiConsoleApp
 {
     class Program
     {
-        static int s1 = 0;
+       // static int s1 = 0;
 
         static void Main(string[] args)
         {
+            //Sayının Karesi ve küp alma
+            int x = 5, c;
+            Console.WriteLine("Sayı : "+x);
+            Console.WriteLine("Sayının karesi : "+kare(x,out c) );
+            Console.WriteLine("Sayının küpü : "+c);
+            //Sayının Karesi ve küp alma son
+
             //Metinlerin En uzun olanını ve karakter sayısını bulma 
-            string metMax;
-            Console.WriteLine("Lütfen Bir Metin1 Giriniz:");
-            string metin1 = Console.ReadLine();
-            Console.WriteLine("Lütfen Bir Metin2 Giriniz:");
-            string metin2 = Console.ReadLine();
-            metinhesapla(metin1, metin2, out metMax,out s1);
-            Console.WriteLine($"Metinlerin en büyüğü {metMax} {s1}");
+            //string metMax;
+            //Console.WriteLine("Lütfen Bir Metin1 Giriniz:");
+            //string metin1 = Console.ReadLine();
+            //Console.WriteLine("Lütfen Bir Metin2 Giriniz:");
+            //string metin2 = Console.ReadLine();
+            //metinhesapla(metin1, metin2, out metMax,out s1);
+            //Console.WriteLine($"Metinlerin en büyüğü {metMax} {s1}");
             //Metinlerin En uzun olanını ve karakter sayısını bulma Son
 
 
@@ -25,25 +32,36 @@ namespace refOutParametresiConsoleApp
             //kucukgetir(a, b, out min);
             //Console.WriteLine($"Sayıların en büyüğü {max},Sayıların en küçüğü {min}");
             //Sayıların En büyüğünü ve En küçüğünü Bulma Son
-            
+
 
 
         }
-        //Metinlerin En uzun olanını ve karakter sayısını bulma Başlangıç
-        private static void metinhesapla(string metin1, string metin2, out string metMax, out int s1)
+        //Sayının Karesi ve küp alma
+        private static int kare(int a,out int y)
         {
-            
-            if (metin1.Length>metin2.Length)
-            {
-                metMax = metin1;
-                s1 = metin1.Length;
-            }
-            else
-            {
-                metMax = metin2;
-                s1 = metin2.Length;
-            }
-            }
+            int k;
+            k = a * a;
+            y = a * a * a;
+            return k;
+        }
+        //Sayının Karesi ve küp alma son
+
+
+        //Metinlerin En uzun olanını ve karakter sayısını bulma Başlangıç
+        //private static void metinhesapla(string metin1, string metin2, out string metMax, out int s1)
+        //{
+
+        //    if (metin1.Length>metin2.Length)
+        //    {
+        //        metMax = metin1;
+        //        s1 = metin1.Length;
+        //    }
+        //    else
+        //    {
+        //        metMax = metin2;
+        //        s1 = metin2.Length;
+        //    }
+        //    }
         //Metinlerin En uzun olanını ve karakter sayısını bulma Son
 
 
